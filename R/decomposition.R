222824be2383e57d9573e56a45b03b46d041317a
vyu <victor.yu@hertfordshire.gov.uk>
2025-09-25 14:26:51 +0000

    checks

--- R/decomposition.R
+++ R/decomposition.R
@@ -4,22 +4,32 @@
 #'
 #' @param df An outputted life table with
 #' @param method Methods to use for life expectancy decomposition. Defaults to 'arriaga3'. Current methods available are: 'arriaga3', 'arriaga2'. Future available methods will include:
-#' @param age_col Column providing ordered age bands. Of factor type.
-#' @param e1 Expectation of life at age group x, in the first group of comparison.
-#' @param e2 Expectation of life at age group x, in the first group of comparison.
-#' @param l1 Expectation of life at age group x, in the first group of comparison.
-#' @param l2 Expectation of life at age group x, in the first group of comparison.
+#' @param age_col Column providing ordered age bands with the final age group being an open-ended interval suffxied with '+', e.g. '90+'.. Of factor type.
+#' @param e1 Column name for expectation of life at age group x, in the 1st group of comparison.
+#' @param e2 Column name for expectation of life at age group x, in the second group of comparison.
+#' @param l1 Column name for The number of persons alive at age group x, in the 1st group of comparison.
+#' @param l2 Column name for The number of persons alive at age group x, in the 1st group of comparison.
+#' @param append Whether to append the decomposition columns to the original data frame.
 #'
-#'
-#' @returns
+#' @returns A data frame with life expectancy decomposition values
 #' @export
 #'
 #' @examples
-decomposition <- function(df, method, age_col, e1, e2, l1, l2, append = TRUE) {
+#' decomposition(us_females, age_col = "Age", e1 = "e1x", e2 = "e2x", l1 = "l1x", l2 = "l2x")
+#'
+decomposition <- function(df, method = "arriaga3", age_col, e1, e2, l1, l2, append = TRUE) {
+  if (!is.factor(df[[age_col]])) stop("The age column is not of type factor")
+
+  age_band_logical <- levels(df[[age_col]]) |>
+    as.vector() |>
+    str_detect("\\+")
+
+  if (age_band_logical |> sum() == 0) stop("No open-ended age band found. The last level must be the sole open-ended age band suffixed with '+'")
+  if (age_band_logical |> sum() > 1) stop("More than one open age band found. The last level must be the sole open age band suffixed with '+'")
+  if (isFALSE(age_band_logical[length(age_band_logical)] && sum(age_band_logical) == 1)) stop("The last age band is not open-ended. Another age band is open-ended.")
 
 
-  if (is.factor(df[["AgeGroup"]])) stop("The age column is not of type factor")
-  if (levels(df[["AgeGroup"]]) |> as.vector() |> str_detect("\\+") |> sum() == 0) stop("")
+
 
 
   df %>% mutate(
