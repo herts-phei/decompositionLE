1a0b37a1394c6d89a996f9e4d2968a2ec6b32e40
vyu <victor.yu@hertfordshire.gov.uk>
2025-10-08 12:48:01 +0000

    ensure df is output

--- tests/testthat/test-decomp_disease.R
+++ tests/testthat/test-decomp_disease.R
@@ -8,3 +8,14 @@ test_that("decomp_disease errors on invalid breakdown", {
     "Invalid breakdown argument selected"
   )
 })
+
+test_that("decomp_disease valid output", {
+  expect_s3_class(
+    decomp_disease(india_china_males_1990,
+      breakdown = "proportion", diseases = c("CD", "NCD", "Injuries"),
+      group_1 = "India", group_1_m = "India_nmx", group_2 = "China",
+      group_2_m = "China_nmx", nDx = "nDx"
+    ),
+    "data.frame"
+  )
+})
