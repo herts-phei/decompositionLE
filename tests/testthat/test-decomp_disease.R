ec5e204de21f44b012fb5d9eaf72d0fe7a2d5cea
vyu <victor.yu@hertfordshire.gov.uk>
2025-10-22 11:13:15 +0000

    more unit tests

--- tests/testthat/test-decomp_disease.R
+++ tests/testthat/test-decomp_disease.R
@@ -1,3 +1,5 @@
+## input validation ------------------------------------------------------------
+
 test_that("decomp_disease errors on invalid breakdown", {
   expect_error(
     decomp_disease(india_china_males_1990,
@@ -9,13 +11,38 @@ test_that("decomp_disease errors on invalid breakdown", {
   )
 })
 
-test_that("decomp_disease valid output", {
-  expect_s3_class(
-    decomp_disease(india_china_males_1990,
+test_that("decomp_disease errors on one disease element not found in groups", {
+  expect_error(
+    decomp_disease(india_china_males_1990[, -3],
       breakdown = "proportion", diseases = c("CD", "NCD", "Injuries"),
       group_1 = "India", group_1_m = "India_nmx", group_2 = "China",
       group_2_m = "China_nmx", nDx = "nDx"
     ),
-    "data.frame"
+    "One or more diseases not found in groups"
   )
+})
+
+## calculations validation -----------------------------------------------------
+
+test_that("calculates delta correctly for raw breakdown", {
+  result <- decomp_disease(india_china_males_1990,
+    breakdown = "proportion", diseases = c("CD"),
+    group_1 = "India", group_1_m = "India_nmx",
+    group_2 = "China", group_2_m = "China_nmx", nDx = "nDx"
+  )
+  expected <- with(india_china_males_1990, nDx * (China_CD - India_CD) / (China_nmx - India_nmx))
+  expect_equal(result$delta_CD, expected)
+})
+
+
+## output validation -----------------------------------------------------------
+
+test_that("decomp_disease valid output", {
+  result <- decomp_disease(india_china_males_1990,
+    breakdown = "proportion", diseases = c("CD", "NCD", "Injuries"),
+    group_1 = "India", group_1_m = "India_nmx", group_2 = "China",
+    group_2_m = "China_nmx", nDx = "nDx"
+  )
+  expect_s3_class(result, "data.frame")
+  expect_true(all(c("delta_CD", "delta_NCD", "delta_Injuries") %in% colnames(result)))
 })
