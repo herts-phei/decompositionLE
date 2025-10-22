8d986e2fac8bbd32f22b900fa4393c337e24b047
wyuill <10546300+wyuill@users.noreply.github.com>
2025-10-22 15:55:55 +0100

    Add files via upload

--- tests/testthat/test-decomp_disease.R
+++ tests/testthat/test-decomp_disease.R
@@ -1,5 +1,3 @@
-## input validation ------------------------------------------------------------
-
 test_that("decomp_disease errors on invalid breakdown", {
   expect_error(
     decomp_disease(india_china_males_1990,
@@ -11,38 +9,50 @@ test_that("decomp_disease errors on invalid breakdown", {
   )
 })
 
-test_that("decomp_disease errors on one disease element not found in groups", {
-  expect_error(
-    decomp_disease(india_china_males_1990[, -3],
+test_that("decomp_disease valid output", {
+  expect_s3_class(
+    decomp_disease(india_china_males_1990,
       breakdown = "proportion", diseases = c("CD", "NCD", "Injuries"),
       group_1 = "India", group_1_m = "India_nmx", group_2 = "China",
       group_2_m = "China_nmx", nDx = "nDx"
     ),
-    "One or more diseases not found in groups"
+    "data.frame"
   )
 })
 
-## calculations validation -----------------------------------------------------
+test_that("checks input is in correct format", {
+  df_test_fct <- india_china_males_1990
+  df_test_fct$Age <- as.character(india_china_males_1990$Age)
+  expect_error(decomp_disease(india_china_males_1990,
+                              breakdown = "proportion", diseases = c("CD", "NCD", "Injuries"),
+                              group_1 = "India", group_1_m = "India_nmx", group_2 = "China",
+                              group_2_m = "China_nmx", nDx = "nDx"))
+  df_test_prop <- india_china_males_1990
+  df_test_prop$l1x[1] <- 2
+  expect_error(decomp_age(df_test_prop, method = "arriaga3", age_col = "Age",
+                          e1 = "e1x", e2 = "e2x", l1 = "l1x", l2 = "l2x"))
+  df_test_num <- india_china_males_1990
+  df_test_num$l2x <- as.character(df_test_num$l2x)
+  expect_error(decomp_age(df_test_num, method = "arriaga3", age_col = "Age",
+                          e1 = "e1x", e2 = "e2x", l1 = "l1x", l2 = "l2x"))
+  #TODO Add check that row props equal 1
 
-test_that("calculates delta correctly for raw breakdown", {
-  result <- decomp_disease(india_china_males_1990,
-    breakdown = "proportion", diseases = c("CD"),
-    group_1 = "India", group_1_m = "India_nmx",
-    group_2 = "China", group_2_m = "China_nmx", nDx = "nDx"
-  )
-  expected <- with(india_china_males_1990, nDx * ((China_CD * China_nmx) - (India_CD * India_nmx)) / (China_nmx - India_nmx))
-  expect_equal(result$delta_CD, expected)
 })
 
+test_that("checks that example data matches the paper", {
 
-## output validation -----------------------------------------------------------
+  preston_box4.3_results <- data.frame(
+    Age = c("0", "5", "15", "30", "45", "60", "70+"),
+    delta_CD = c(5.6, 0.8, 0.3, 0.6, 0.8, 0.3, -0.3),
+    delta_NCD = c(5.5, 0.6, 0.4, 0.6, 0.7, 0.5, 0.7),
+    delta_Injuries = c(-0.0, 0.2, -0.0, 0.1, 0.1, -0.0, -0.1))
 
-test_that("decomp_disease valid output", {
-  result <- decomp_disease(india_china_males_1990,
-    breakdown = "proportion", diseases = c("CD", "NCD", "Injuries"),
-    group_1 = "India", group_1_m = "India_nmx", group_2 = "China",
-    group_2_m = "China_nmx", nDx = "nDx"
-  )
-  expect_s3_class(result, "data.frame")
-  expect_true(all(c("delta_CD", "delta_NCD", "delta_Injuries") %in% colnames(result)))
+  df_test <- decomp_disease(india_china_males_1990,
+                            breakdown = "proportion", diseases = c("CD", "NCD", "Injuries"),
+                            group_1 = "India", group_1_m = "India_nmx", group_2 = "China",
+                            group_2_m = "China_nmx", nDx = "nDx") |>
+    select(Age, delta_CD, delta_NCD, delta_Injuries)
+
+  expect_equal(df_test, preston_box4.3_results)
+
 })
